e3c702af3a09e70f9cff615d1d8cdbe4b046047d
sakii-07 <sakshijagtap9304@gmail.com>
2026-02-12 17:39:03 +0530

    feb12 tast-1

--- feb12.py
+++ feb12.py
@@ -13,5 +13,21 @@ print(s[-1])
 print("s[len(s)-1] :",s[len(s)-1])
 print("middle character",s[len(s)//2]) # middle character
 
-print(s[12]) # Throws error - IndexError: string index out of range
-print(s[-12])
+# print(s[12]) # Throws error - IndexError: string index out of range
+# print(s[-12])
+
+# Task 1
+
+# Accept any string from console
+str = input("Enter the any string ")
+
+# 1. display length of the string 
+print("length of give string : ", len(str))
+
+# 2. display first and last index of string
+print("first index of given string : ", 0) # display first index
+print("last index of given string using str[len(str)-1] : ", len(str)-1) # display last index
+
+# 3. accept any index from user and display character present at that index
+index = int(input("Enter index number : "))
+print("Character index at given index : ", str[index])
