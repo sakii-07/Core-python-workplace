c0f9524b3e9f29d60c651d703d2918280a498191
sakii-07 <sakshijagtap9304@gmail.com>
2026-03-04 21:58:46 +0530

    completed

---
+++ Core Python/mar4.py
@@ -0,0 +1,121 @@
+'''
+Dictionary :- Dictionary is mutable, heterogenious collection of element where order is not 
+              maintained(before python 3.7) , now it is ordered (after 3.7+) and values are stored 
+              in key-value pair. 
+
+              Keys are unique in nature, values may be duplicate.
+
+              key : value
+
+              key must be immutable datatype, values may mutable or immutable.
+
+              immutable datatypes :- int, float, complex, bool, string,    tuple, frozen set
+
+Q. how to create empty dict?
+  varname = {}
+
+Q. how to add data into dictionary?
+--> var[key] = value # {key : value}
+
+methods of dictionary ----> 1) get() - access single element in dictionary
+                            2) keys() - returns the all keys of dictionary
+                            3) values() - returns the all values of dictionary
+                            4) items() - returns the all keys and value of dictionary
+'''
+# level o dictionary
+stud_db = {}
+stud_db[1]="sakshi"
+stud_db[2]="amruta"
+stud_db[3]="divya"
+stud_db[4]="pranjali"
+stud_db[5]="sojar"
+print(stud_db) # {1: 'sakshi', 2: 'amruta', 3: 'divya', 4: 'pranjali', 5: 'sojar'}
+
+'''
+Q. how to access single element in dictionary?
+--> by using key or get method
+'''
+# using key value
+v1 = stud_db[1]
+print(v1) # sakshi
+
+# using get method
+v2 = stud_db.get(3)
+print(v2) # divya
+
+v1 = stud_db[30]
+print(v1) # KeyError: 30
+
+# using get method
+v2 = stud_db.get(30)
+print(v2) # None
+
+'''
+keys() :- keys() will return all keys from dictionary
+'''
+
+# Iterating over key
+# way 1 : to display key
+for i in stud_db:
+    print(i)
+
+# way 2 : to display key
+for i in stud_db.keys():
+    print(i)
+'''
+1
+2
+3
+4
+5
+''' 
+'''
+values() :- values() will return all values from dictionary
+'''
+# way 1 : dislpay all values
+for i in stud_db:
+    print(i,"-->",stud_db.get(i))
+
+# way 2 : display all values
+for i in stud_db:
+    print(stud_db.values())
+
+for i in stud_db.values():
+    print(i)
+'''
+1 --> sakshi
+2 --> amruta
+3 --> divya
+4 --> pranjali
+5 --> sojar
+'''
+
+# way 3 :- display keys as well as values
+for k in stud_db:
+    print(k,stud_db[k])
+  
+'''
+items() meethod :- return all key and values in tuple
+'''
+for k in stud_db.items():
+  print(k)
+'''
+(1, 'sakshi')
+(2, 'amruta')
+(3, 'divya')
+(4, 'pranjali')
+(5, 'sojar')
+'''
+for t in stud_db.items():
+  k,v = t
+  print(k,"---->",v)
+
+for k,v in stud_db.items():
+  print(k,"---->",v)
+'''
+1 ----> sakshi
+2 ----> amruta
+3 ----> divya
+4 ----> pranjali
+5 ----> sojar
+'''
---
+++ Homework/dict.py
@@ -0,0 +1,123 @@
+'''
+create dictionary of 2025 movies
+'''
+movie_db = {}
+
+chhaava_cast = ["Vicky Kaushal", "Rashmika Mandanna", "Akshaye Khanna", "Ashutosh Rana", "Divya Dutta", "Rajesh Sharma"] 
+
+dhurandar_cast = ["Ranveer Singh", "Akshaye Khanna", "Sanjay Dutt", "R. Madhavan", "Arjun Rampal", "Sara Arjun", "Rakesh Bedi", "Gaurav Gera", "Danish Pandor"]
+
+salaar_cast = ["Prabhas", "Prithviraj Sukumaran", "Shruti Haasan", "Jagapathi Babu", "Bobby Simha", "Sriya Reddy"]
+
+singham_again_cast = ["Ajay Devgn", "Kareena Kapoor Khan", "Deepika Padukone", "Ranveer Singh", "Akshay Kumar", "Tiger Shroff"]
+
+movie_db["Chhaava"] = chhaava_cast
+movie_db["Dhurandar"] = dhurandar_cast
+movie_db["Salaar"] = salaar_cast
+movie_db["Singham_again"] = singham_again_cast
+
+# Display the complete dictionary
+print(movie_db)
+'''
+{'Chhaava': ['Vicky Kaushal', 'Rashmika Mandanna', 'Akshaye Khanna', 'Ashutosh Rana', 'Divya Dutta',
+ 'Rajesh Sharma'], 'Dhurandar': ['Ranveer Singh', 'Akshaye Khanna', 'Sanjay Dutt', 'R. Madhavan', 
+ 'Arjun Rampal', 'Sara Arjun', 'Rakesh Bedi', 'Gaurav Gera', 'Danish Pandor'],
+'Salaar': ['Prabhas', 'Prithviraj Sukumaran', 'Shruti Haasan', 'Jagapathi Babu', 'Bobby Simha', 
+'Sriya Reddy'], 'Singham_again': ['Ajay Devgn', 'Kareena Kapoor Khan', 'Deepika Padukone', 'Ranveer Singh',
+ 'Akshay Kumar', 'Tiger Shroff']}
+'''
+# Display the all keys of dictionary
+for k in movie_db.keys():
+    print(k)
+'''
+Chhaava
+Dhurandar
+Salaar
+Singham_again
+'''
+
+# Display the all values of dictionary
+for v in movie_db.values():
+    print(v)
+'''
+['Vicky Kaushal', 'Rashmika Mandanna', 'Akshaye Khanna', 'Ashutosh Rana', 'Divya Dutta', 'Rajesh Sharma']
+['Ranveer Singh', 'Akshaye Khanna', 'Sanjay Dutt', 'R. Madhavan', 'Arjun Rampal', 'Sara Arjun', 'Rakesh Bedi', 'Gaurav Gera', 'Danish Pandor']
+['Prabhas', 'Prithviraj Sukumaran', 'Shruti Haasan', 'Jagapathi Babu', 'Bobby Simha', 'Sriya Reddy']
+['Ajay Devgn', 'Kareena Kapoor Khan', 'Deepika Padukone', 'Ranveer Singh', 'Akshay Kumar', 'Tiger Shroff']
+'''
+
+# Display the all items (key with value) of dictionary
+for t in movie_db.items():
+    print(t)
+'''
+('Chhaava', ['Vicky Kaushal', 'Rashmika Mandanna', 'Akshaye Khanna', 'Ashutosh Rana', 'Divya Dutta', 'Rajesh Sharma'])
+('Dhurandar', ['Ranveer Singh', 'Akshaye Khanna', 'Sanjay Dutt', 'R. Madhavan', 'Arjun Rampal', 'Sara Arjun', 'Rakesh Bedi', 'Gaurav Gera', 'Danish Pandor'])
+('Salaar', ['Prabhas', 'Prithviraj Sukumaran', 'Shruti Haasan', 'Jagapathi Babu', 'Bobby Simha', 'Sriya Reddy'])
+('Singham_again', ['Ajay Devgn', 'Kareena Kapoor Khan', 'Deepika Padukone', 'Ranveer Singh', 'Akshay Kumar', 'Tiger Shroff'])
+'''
+
+# Display the all keys and values of dictionary
+for k,v in movie_db.items():
+    print(k,"--->",v)
+'''
+Chhaava ---> ['Vicky Kaushal', 'Rashmika Mandanna', 'Akshaye Khanna', 'Ashutosh Rana', 'Divya Dutta', 'Rajesh Sharma']
+Dhurandar ---> ['Ranveer Singh', 'Akshaye Khanna', 'Sanjay Dutt', 'R. Madhavan', 'Arjun Rampal', 'Sara Arjun', 'Rakesh Bedi', 'Gaurav Gera', 'Danish Pandor']
+Salaar ---> ['Prabhas', 'Prithviraj Sukumaran', 'Shruti Haasan', 'Jagapathi Babu', 'Bobby Simha', 'Sriya Reddy']
+Singham_again ---> ['Ajay Devgn', 'Kareena Kapoor Khan', 'Deepika Padukone', 'Ranveer Singh', 'Akshay Kumar', 'Tiger Shroff']
+'''
+
+# Display the all keys of dictionary using items() method
+for k,v in movie_db.items():
+    print(k)
+'''
+Chhaava
+Dhurandar
+Salaar
+Singham_again
+'''
+
+# task :- Display the names of the movies of 'Ranveer Singh' and the total number of movies.
+count = 0
+for k,v in movie_db.items():
+    for i in v:
+        if i == "Ranveer Singh":
+            count = count + 1
+            print(k)
+print("The number of movies : ",count)
+'''
+Dhurandar
+Singham_again
+The number of movies :  2
+'''
+
+# task :- Display the number of 'a' in movie_db
+count = 0
+for k,v in movie_db.items():
+    for i in v:
+        for l in i:
+            if l.lower() == 'a':
+                count = count + 1
+print(count) # 67
+
+# task :- Display the number of actors whose names start with 'A' in movie_db.
+count = 0
+for k,v in movie_db.items():
+    for i in v:
+        for name in i:
+            if name.startswith('A'):
+                count=count+1
+print(count) # 7
+
+# Task :- Display the names of the movies of 'Akshaye Khanna' and the total number of movies.
+count = 0
+for k,v in movie_db.items():
+    for name in v :
+        if name == "Akshaye Khanna":
+            count = count + 1
+            print(k)
+print("The number of movies : ", count)
+'''
+Chhaava
+Dhurandar
+The number of movies :  2
+'''
