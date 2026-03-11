0f22bf47781519aaff25d275eddce72f926c37f6
sakii-07 <sakshijagtap9304@gmail.com>
2026-03-11 23:21:28 +0530

    -

---
+++ Core Python/mar11.py
@@ -0,0 +1,91 @@
+'''
+DRY - donot repeat yourself (py )
+loop - to repetative task
+2) Iterative statement
+                  1) for loop - Entry control loop - If we know number of iteration in advance
+                    syntax - for varname in sequence :
+                                 body of for loop
+    Note - py internally finds the lenght of sequence
+                  2) while loop - If we don't know number of iteration in advance
+'''
+
+# # WAP to print 1 to 5 on console using for loop
+# for i in range(1,6):
+#     print(i) # 1 2 3 4 5
+
+# for i in range(101,106):
+#     print(i-100)
+
+# for i in [1,2,3,4,5]:
+#     print(i)
+
+# for i in (1,2,3,4,5):
+#     print(i)
+
+# # WAP to print 100 to 5 on console using for loop
+# for i in range(100,4,-1):
+#     print(i)
+
+# # WAP to print all odd number from 23 to 67 on console using for loop
+# for i in range(23,68,2):
+#     print(i)
+
+# for i in range(23,68):
+#     if i % 2 != 0:
+#         print(i)
+
+# for i in range(23,68):
+#     if i % 2 == 1:
+#         print(i)
+
+# 
+# prime number - the number is divisible by 1 and itself
+# way-1
+num = 7
+count = 0
+
+for i in range(1,num+1):
+    if num % i == 0:
+        count += 1
+if count == 2 :
+    print(num, "is a prime number")
+else :
+    print(num, "is a not prime number")
+
+# way-2
+num = 7
+count = 0
+
+for i in range(1,num+1):
+    if num % i == 0:
+        count += 1
+if count == 0 :
+    print(num, "is a prime number")
+else :
+    print(num, "is a not prime number")
+
+# way-3
+num = 7
+count = 0
+
+for i in range(2,num//2 + 1):
+    if num % i == 0:
+        count += 1
+if count == 0 :
+    print(num, "is a prime number")
+else :
+    print(num, "is a not prime number")
+
+# way-4
+num = 7
+count = 0
+
+for i in range(2,num//2 + 1):
+    if num % i == 0:
+        count += 1
+        break
+if count == 0 :
+    print(num, "is a prime number")
+else :
+    print(num, "is a not prime number")
+
---
+++ Homework/primeNumber.py
@@ -0,0 +1,112 @@
+# WAP to check given number is prime or not ?
+
+# prime number way - 1
+num = 190
+count = 0
+
+for i in range(1,num+1):
+    if num % i == 0:
+        count += 1
+
+if count == 2:
+    print(num,"is a prime number")
+else:
+    print(num,"is not prime number") # 190 is not prime number
+
+# prime number way - 2
+num = 29
+count = 0
+
+for i in range(2,num):
+    if num % i == 0:
+        count += 1
+
+if count == 0:
+    print(num,"is a prime number") # 29 is a prime number
+else :
+    print(num,"is a not prime number")
+
+# prime number way - 3
+num = 67
+count = 0
+
+for i in range(2,num // 2 +1):
+    if num % i == 0:
+        count += 1
+
+if count == 0:
+    print(num,"is a prime number") # 67 is a prime number
+else :
+    print(num,"is a not prime number")
+
+# prime number way - 4
+num = 98
+count = 0
+
+for i in range(2,num//2 + 1):
+    if num % i == 0:
+        count += 1
+        break         
+
+if count == 0:
+    print(num,"is a prime number")
+else :
+    print(num,"is not a prime number") # 98 is not a prime number
+    
+
+# find prime number between 1 to 20
+
+# way - 1
+for num in range(1,20):
+    count = 0
+    for i in range(1,num+1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        print(num)
+
+# way - 2
+for num in range(2,21):
+    count = 0
+    for i in range(2, num):
+        if num % i == 0:
+            count += 1
+    if count == 0:
+        print(num)
+
+# way - 3
+for num in range(2,21):
+    count = 0
+    for i in range(2,num//2 +1):
+        if num % i == 0:
+            count += 1
+    if count == 0:
+        print(num)
+'''
+2
+3
+5
+7
+11
+13
+17
+19
+'''
+
+# way - 4
+for num in range(10,31):
+    count = 0
+    for i in range(2,num//2 + 1):
+        if num % i == 0:
+            count += 1
+            break
+    if count == 0:
+        print(num)
+'''
+11
+13
+17
+19
+23
+29
+'''
