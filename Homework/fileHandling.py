da4b92f4ae7cbc56350a4471d19b0305a1456ee0
sakii-07 <sakshijagtap9304@gmail.com>
2026-04-15 15:11:21 +0530

    15 apr homework

--- Homework/fileHandling.py
+++ Homework/fileHandling.py
@@ -2,18 +2,30 @@
 homework
 copy content of one file into another file
 '''
-try:
+def copy_file(src_file, dest_file):
+
+    try:
     # open source file in read mode
-    with open("Homework/data.txt", "r") as src:
-        data = src.read()
+        with open(src_file, "r") as src:
+            data = src.read()
 
-    # open destination file in write mode
-    with open("Homework/data_copy.txt", "w") as dest:
-        dest.write(data)
+        # open destination file in write mode
+        with open(dest_file, "w") as dest:
+            dest.write(data)
 
-    print("File copied successfully!")
+        print("File copied successfully!")
 
-except Exception as e:
-    print("Error occurred:", e)
+    except Exception as e:
+        print("Error occurred:", e)
 
-    
+# taking input from user
+source = input("Enter source file path: ")
+destination = input("Enter destination file path: ")
+
+# passing arguments to function
+copy_file(source, destination)
+'''
+Enter source file path: Homework/data.txt
+Enter destination file path: Homework/copy_data1.txt
+File copied successfully!
+'''   
