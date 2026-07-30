1b68e4b5d1cfcfefd0467cd832c4278d4a7c5744
sakii-07 <sakshijagtap9304@gmail.com>
2026-07-30 10:02:15 +0530

    30-july-2026

---
+++ programming batch/jule07.py
@@ -0,0 +1,27 @@
+# l1 = []
+# print(type(l1))
+# num = int(input("Enter the number : "))
+# l1.append(num)
+# print(l1)
+
+# t1 = ()
+# print(type(t1))
+
+# s1 = set()
+# print(type(s1))
+# print(id(s1))
+# string = input("Enter the string : ")
+# s1.add(string)
+# print(s1)
+
+# d = {}
+# print(type(d))
+# print(id(d))
+
+l2 = [1,2,3,4,5]
+for i in l2:
+    print(i)
+
+# WAP to print numbers 1-5
+for i in range(1,6):
+    print(i)
